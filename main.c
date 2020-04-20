fd7447371af10ca876c645d5035a771c8546cc27
Bruno <11825803+bbanho@users.noreply.github.com>
2020-04-20 19:20:46 -0300

    generation of noisy circle images

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define DEF_LARG 800
+#define DEF_ALT 600
 
 typedef struct Pgm {
   unsigned int alt,larg;
@@ -7,16 +11,59 @@ typedef struct Pgm {
   unsigned char **data;
 } Pgm;
 
+Pgm generateNoise(Pgm img);
 Pgm binariza(Pgm img, unsigned int c);
 Pgm normaliza(Pgm img);
 Pgm loadPgm(char *filename);
 int savePgm(Pgm img, unsigned int c, char *filename);
+Pgm drawCircle(Pgm img,unsigned int x,unsigned int y,unsigned int r);
 
 int main(int argc, char *argv[]){
-  Pgm input;
-  input = loadPgm(argv[1]);
-  savePgm(normaliza(binariza(input,1)),255,argv[2]);
+
+  // initiate (and save) blank image
+  Pgm img = loadPgm("img.pgm");
+
+  // generates a circle (x,y,r)
+  Pgm circle = drawCircle(img,400,300,40);
+  // add noise
+  Pgm circle_noise = generateNoise(circle);
+  savePgm(circle_noise,255,"newcircle.pgm");
+
+//  Pgm noise = generateNoise(loadPgm(argv[1]));
+//  savePgm(noise,255,"noise.pgm");
+//
+//  // draw circle over noise
+//  Pgm noise_circle = drawCircle(noise,100,100,50);
+//  savePgm(noise_circle,255,"noise_circle.pgm");
+
+  // tries to remove noise around circle
+
   return 0;
+}
+
+Pgm generateNoise(Pgm img){
+  srand(123);
+  for(unsigned int i=0;i<img.alt;i++){
+    for(unsigned int j=0;j<img.larg;j++){
+      img.data[i][j]=(3*img.data[i][j]+rand()%255)/4;
+    }
+  }
+  return img;
+}
+
+Pgm drawCircle(Pgm img,unsigned int x,unsigned int y,unsigned int r){
+
+  if(x<img.larg && y<img.alt && r>0){
+    // x**2 + y**2 = r**2
+    for(unsigned int i=0;i<img.alt;i++){
+      for(unsigned int j=0;j<img.larg;j++){
+        if((x-i)*(x-i) + (y-j)*(y-j) <= r*r) img.data[i][j]=255;
+        // (x-i)**2 + (y-j)**2 = r**2
+      }
+    }
+  }
+
+  return img;
 }
 
 Pgm binariza(Pgm img,unsigned int c){
@@ -33,7 +80,6 @@ Pgm binariza(Pgm img,unsigned int c){
 }
 
 Pgm normaliza(Pgm img){
-  unsigned char ker[3][3];
   float pix_s=0;
   for(unsigned int i=1;i<img.alt-1;i++){
     for(unsigned int j=1;j<img.larg-1;j++){
@@ -54,8 +100,8 @@ Pgm normaliza(Pgm img){
 
 Pgm loadPgm(char *filename){
   Pgm img;
-  FILE *f = NULL;
-  f=fopen(filename, "rb");
+  FILE *f;
+  f=fopen(filename, "w+b");
   fscanf(f, "%s\n", img.magic);
   if(img.magic[1]=='5'){
     fscanf(f,"%d",&img.larg);
@@ -64,6 +110,15 @@ Pgm loadPgm(char *filename){
     for(unsigned int i=0;i<img.alt;i++){
       img.data[i]=calloc(img.larg,img.larg*sizeof(char));
       for(unsigned int j=0;j<img.larg;j++) fscanf(f,"%c",&img.data[i][j]);
+    }
+  } else {
+    img.larg=DEF_LARG; img.alt=DEF_ALT;
+    strcpy(img.magic,"P5");
+    fprintf(f,"%s %d %d\n%d\n",img.magic,img.larg,img.alt,255);
+    img.data=calloc(img.alt,img.alt*sizeof(char*));
+    for(unsigned int i=0;i<img.alt;i++){
+      img.data[i]=calloc(img.larg,img.larg*sizeof(char));
+      for(unsigned int j=0;j<img.larg;j++) fprintf(f,"%c",img.data[i][j]);
     }
   }
   fclose(f);
@@ -82,3 +137,4 @@ int savePgm(Pgm img, unsigned int c, char *filename){
   fclose(f);
   return 0;
 }
+
